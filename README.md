e4ecf95e797e8e07a1754e088b917ae0fe0fd705
zk-v <zakodos.vesna@gmail.com>
2019-11-07 19:30:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-api-request
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-api-request)
---
+++ index.js
@@ -0,0 +1,49 @@
+// Import stylesheets
+import './style.css';
+
+
+// Write Javascript code!
+const app = document.getElementById('app');
+
+// Create a request variable and assign a new XMLHttpRequest object to it.
+var request = new XMLHttpRequest();
+
+// Open a new connection, using the GET request on the URL endpoint
+request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+
+request.onload = function() {
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response);
+  
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(movie => {
+      const card = document.createElement('div');
+      card.classList.add('card');
+      const cardBody = document.createElement('div');
+      cardBody.classList.add('card-body');
+      const title = document.createElement('h2');
+      title.textContent = movie.title;
+      const description = document.createElement('p');
+      description.textContent = movie.description;
+
+      const image = document.createElement('img');
+      // Create a random number between 0 and 200
+      let randNum = Math.floor(Math.random() * 201);
+      console.log(randNum)
+      let src = `https://picsum.photos/id/${randNum}/500/200`;
+      image.setAttribute('src', src);
+
+      app.appendChild(card);
+      card.appendChild(cardBody);
+      cardBody.appendChild(image);
+      cardBody.appendChild(title);
+      cardBody.appendChild(description);
+
+    })
+  } else {
+    console.log('error');
+  }
+}
+
+// Send request
+request.send();
